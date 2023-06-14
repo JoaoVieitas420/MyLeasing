7d1eed550b63fd956146bc3c839a61ef35106738
joaovieitas420 <joao.vieitas2@gmail.com>
2023-06-14 16:08:35 +0100

    API

---
+++ MyLeasing.Web/Controllers/API/OwnersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyLeasing.Web.Data;
+
+namespace MyLeasing.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnersController : Controller
+    {
+        private readonly IOwnersRepository _productRepository;
+
+        public OwnersController(IOwnersRepository ownerRepository)
+        {
+
+            _productRepository = ownerRepository;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetOwners()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+    }
+}
